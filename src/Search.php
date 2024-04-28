12842269b365d133c1f602467425428fa802697e
Denis Souza <denis.souza@ulteriorti.com.br>
2024-04-28 19:31:21 -0300

    hardening search vars

--- src/Search.php
+++ src/Search.php
@@ -1,11 +1,17 @@
 <?php
 declare(strict_types=1);
 namespace Ulteriorti\Digitalcep;
+/*
+*Realiza busca do CEP no VIACEP
+*/
 class Search{
     private string $url = "https://viacep.com.br/ws/";
+    private int $zipCodeInt;
     public function getAddressFromZipCode(string $zipCode):array{
         $zipCode = preg_replace('/[^0-9]/im','',$zipCode);
-        $get = file_get_contents($this->url.$zipCode."/json");
+        $zipCodeInt = intval($zipCode);
+        $zipCodeInt = filter_var($zipCode,FILTER_VALIDATE_INT);
+        $get = file_get_contents($this->url.$zipCodeInt."/json");
         //print_r($get);
         return (array) json_decode($get);
 
