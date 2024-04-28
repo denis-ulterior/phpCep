59200d3e08ca7cc77aa78f5c139e0eec8d2891b1
Denis Souza <denis.souza@ulteriorti.com.br>
2024-04-28 20:10:23 -0300

    fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ulteriorti/digitalcep',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1cf6e01db8110b077ac528f666bdc137386138d3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ulteriorti/digitalcep' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1cf6e01db8110b077ac528f666bdc137386138d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
