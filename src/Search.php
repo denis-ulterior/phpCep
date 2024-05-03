11ebbbd3a79e6c30e2bcd792387232a40a612e5d
Denis Souza <denis.souza@ulteriorti.com.br>
2024-05-02 21:00:10 -0300

    PSR 12 Aplicado

--- src/Search.php
+++ src/Search.php
@@ -1,33 +1,35 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Ulteriorti\Digitalcep;
 
 use Exception;
-use PhpParser\Node\Expr\Throw_;
 
 /*
 *Realiza busca do CEP no VIACEP
 */
-class Search{
+class Search
+{
     private const URL = 'https://viacep.com.br/ws/';
     private int $zipCodeInt;
-    public function getAddressFromZipCode(string $zipCode):array{
-        $zipCode = preg_replace('/[^0-9]/im','',$zipCode);
+    public function getAddressFromZipCode(string $zipCode): array
+    {
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
         $zipCodeInt = intval($zipCode);
-        $zipCodeInt = filter_var($zipCode,FILTER_VALIDATE_INT);
+        $zipCodeInt = filter_var($zipCode, FILTER_VALIDATE_INT);
         $retorno = $this->obterDados($zipCodeInt);
-        return (array) json_decode($retorno);
-
+        return(array) json_decode($retorno);
     }
-    private function obterDados(int $zipCodeInt) : string {
-        try{
-
-        $get = @file_get_contents(self::URL.$zipCodeInt."/json");
-        if ($get === false){
-            throw new Exception("Falha ao obter dados com a entrada informada");
-        }
-        } catch (Exception $e){
-            return ("{".'"'.$zipCodeInt.'" :'. '"'.$e->getMessage().'"'."}");
+    private function obterDados(int $zipCodeInt): string
+    {
+        try {
+            $get = @file_get_contents(self::URL . $zipCodeInt . "/json");
+            if ($get === false) {
+                throw new Exception("Falha ao obter dados com a entrada informada");
+            }
+        } catch (Exception $e) {
+            return ("{" . '"' . $zipCodeInt . '" :' . '"' . $e->getMessage() . '"' . "}");
         }
         return $get;
     }
